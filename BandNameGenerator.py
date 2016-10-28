62943b9d9e0748ebfe9dc7e6e611ef6402506f40
Tyler <tamano@chadwickschool.org>
2016-10-28 11:34:13 -0700

    Add items for list

---
+++ BandNameGenerator.py
@@ -0,0 +1,6 @@
+import random
+adjective=['smelly','rancid','bleeding','condemned','dead','holy','unholy','weeping','guilty','fallen','dark','screaming','black','bloody','guilty'];
+noun1=['witch','skull','lucifer','death','satan','magic','rage','sin','fury','blood','carnage','poop'];
+noun2=['eaters','empire','anarchy','henchmen','army','order','goblins','realm','hate','zombies','angels','warriors'];
+
+
